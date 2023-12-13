2e20fa50ed1b82e8a7a39f9f4ec822e9a8f17f30
jeiji <jeiji@gmail.com>
2023-12-13 12:39:43 +0400

    Add project files.

---
+++ APITest/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class DB
+{
+
+	public bool CheckExists(int userID)
+	{
+		string connectionString = "Data Source=DESKTOP-IBERPBJ;Initial Catalog=TEST;Integrated Security=True;Encrypt=False";
+
+		string move = "SELECT COUNT(*) from SilknetUsers WHERE CustomerID = @userid";
+
+		using (SqlConnection conn = new SqlConnection(connectionString))
+		{
+			conn.Open();
+
+			using (SqlCommand selectUser =  new SqlCommand(move,conn))
+			{
+				selectUser.Parameters.AddWithValue("@userid", userID);
+
+				int userCount = (int)selectUser.ExecuteScalar();
+				
+				if(userCount > 0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+
+
+			}
+		
+
+
+		}
+
+	}
+
+
+
+
+}
---
+++ APITest/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		CreateWebHostBuilder(args).Build().Run();
+		
+	}
+
+	public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+		WebHost.CreateDefaultBuilder(args)
+			.ConfigureServices(services =>
+			{
+				services.AddMvc();
+			})
+			.Configure(app =>
+			{
+				app.UseRouting();
+				app.UseEndpoints(endpoints =>
+				{
+					endpoints.MapControllers();
+				});
+			});
+}
+
+[ApiController]
+public class MyController : ControllerBase
+{
+	[HttpGet("/api/getCorrect")]
+	
+	public IActionResult getValid()
+	{
+		string a = "2001";
+		bool isValid = CheckCharacters(a);
+
+		int ID;
+		DB data = new DB();
+		var status = Response.StatusCode;
+		if (!isValid || !int.TryParse(a, out ID))
+		{
+			status = 400;
+			return BadRequest(new {Message = "YOUR ID MUST CONTAIN ONLY DIGITS",StatusCode = "Can't ADD ID with Characters"});
+
+		}
+		else if (data.CheckExists(ID))
+		{
+			return BadRequest(new { Message = "THIS ID ALREADY EXISTS", StatusCode = "already Exists" });
+		}
+		else
+		{
+			status = 200;
+			return Ok(new { Message = $"Your ID({ID}) ADDED SUCCESSFULLY",StatusCode = $"Success"});
+		}
+		
+
+	}
+	
+
+	private bool CheckCharacters(string a)
+	{
+		for(int i = 0; i < a.Length; i++)
+		{
+			if (a[i] < '0' || a[i] > '9')
+				return false;
+		}
+		return true;
+	}
+	
+}
+
