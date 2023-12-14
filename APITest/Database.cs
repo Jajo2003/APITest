0cd766e802cf3ab1f903d30614eaab36e4ca2454
jeiji <jeiji@gmail.com>
2023-12-14 12:18:06 +0400

    modified

--- APITest/Database.cs
+++ APITest/Database.cs
@@ -31,10 +31,8 @@ public class DB
 
 
 			}
-		
-
-
 		}
+
 
 	}
 
--- APITest/Program.cs
+++ APITest/Program.cs
@@ -34,38 +34,38 @@ class Program
 [ApiController]
 public class UserControler : ControllerBase
 {
-	[HttpGet("/api/getCorrect")]
+	[HttpGet("/api/getExists")]
 	
 	public IActionResult getValid()
 	{
-		string a = "2001";
+		string a = "10022";
 		bool isValid = CheckCharacters(a);
 
 		int ID;
 		DB data = new DB();
 		var status = Response.StatusCode;
+
 		if (!isValid || !int.TryParse(a, out ID))
 		{
 			status = 400;
 			Response.StatusCode = status;
-			return BadRequest(new {Message = "YOUR ID MUST CONTAIN ONLY DIGITS",StatusCode = "Can't ADD ID with Characters"});
+			return BadRequest(new {Message = "Error wrong Characters ",StatusCode = "Error" , Value =a});
 
 		}
-		else if (data.CheckExists(ID))
+		else if (!data.CheckExists(ID))
 		{
 			status = 400;
 			Response.StatusCode = status;
-			return BadRequest(new { Message = "THIS ID ALREADY EXISTS", StatusCode = "already Exists" });
+			return BadRequest(new { Message = "Doesn't exists", StatusCode = "Error", Value = a});
 		}
 		else
 		{
 			status = 200;
 			Response.StatusCode = status;
-			return Ok(new { Message = $"Your ID({ID}) ADDED SUCCESSFULLY",StatusCode = $"Success"});
+			return Ok(new { Message = $"Found",StatusCode = $"Success", Value = a });
 		}
-		
-
 	}
+	
 	
 
 	private bool CheckCharacters(string a)
